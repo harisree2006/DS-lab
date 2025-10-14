cf610d9bb5caf80e04ed909c8e7f0e92f7eeae77
HARISREE S <shemol.suresh@gmail.com>
2025-10-15 04:03:36 +0530

    Create binary.c

--- binary.c
+++ binary.c
@@ -1,58 +1,58 @@
 #include <stdio.h>
 
 void read_array(int n, int arr[]);
-void print_array(int n, int arr[]);
-void search(int key, int arr[], int n);
+void binary_search(int arr[], int n, int key);
 
-int main() {
+void main()
+{
     int n, key;
     int arr[10];
 
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    printf("Enter elements in sorted order: ");
     read_array(n, arr);
 
-    print_array(n, arr);
-
-    printf("\nEnter the element to search: ");
+    printf("Enter the key to be searched: ");
     scanf("%d", &key);
 
-    search(key, arr, n);
-
-    return 0;
+    binary_search(arr, n, key);
 }
 
-void read_array(int n, int arr[]) {
+void read_array(int n, int arr[])
+{
     int i;
-    for(i = 0; i < n; i++)
+    printf("Enter the elements in sorted order:\n");
+    for (i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
+    }
 }
 
-void print_array(int n, int arr[]) {
-    int i;
-    printf("The array is: ");
-    for(i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-}
+void binary_search(int arr[], int n, int key)
+{
+    int begin = 0, end = n - 1, mid, found = 0;
 
-void search(int key, int arr[], int n) {
-    int low = 0, high = n - 1, mid, found = 0;
+    while (begin <= end)
+    {
+        mid = (begin + end) / 2;
 
-    while(low <= high) {
-        mid = (low + high) / 2;
-        if(arr[mid] == key) {
+        if (arr[mid] == key)
+        {
+            printf("Key found at position: %d\n", mid + 1);
             found = 1;
             break;
-        } else if(key < arr[mid])
-            high = mid - 1;
+        }
+        else if (arr[mid] < key)
+        {
+            begin = mid + 1;
+        }
         else
-            low = mid + 1;
+        {
+            end = mid - 1;
+        }
     }
 
-    if(found == 1)
-        printf("\nSearch successful. Key found at position %d.", mid + 1);
-    else
-        printf("\nSearch unsuccessful. Key not found.");
+    if (found == 0)
+        printf("Key not found.\n");
 }
