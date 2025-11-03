d702deb1355b9bc6d0dc6d306ee9f0b6000a66b6
HARISREE S <shemol.suresh@gmail.com>
2025-11-03 19:13:26 +0530

    linear search

--- linear.c
+++ linear.c
@@ -1,54 +1,46 @@
 #include <stdio.h>
 
-void read_array(int n, int arr[]);
-void print_array(int n, int arr[]);
-void search(int key, int arr[], int n);
+void read_array(int, int[]);
+void search(int, int[], int);
 
-int main() {
+void main()
+{
     int n, key;
-    int arr[10];
-
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
+    int arr[10];
     read_array(n, arr);
-    print_array(n, arr);
 
-    printf("\nEnter the element to search: ");
+    printf("Enter the key to be searched: ");
     scanf("%d", &key);
 
-    search(key, arr, n);
-
-    return 0;
+    search(n, arr, key);
 }
 
-void read_array(int n, int arr[]) {
+void read_array(int n, int arr[])
+{
     int i;
-    printf("Enter the elements: ");
-    for(i = 0; i < n; i++)
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++)
+    {
         scanf("%d", &arr[i]);
+    }
 }
 
-void print_array(int n, int arr[]) {
-    int i;
-    printf("The array is: ");
-    for(i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-}
-
-void search(int key, int arr[], int n) {
-    int i, found = 0, loc = -1;
-
-    for(i = 0; i < n; i++) {
-        if(arr[i] == key) {
+void search(int n, int arr[], int key)
+{
+    int i, found = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            printf("Key found at position: %d\n", i + 1);
             found = 1;
-            loc = i;
             break;
         }
     }
 
-    if(found == 0)
-        printf("\nSearch unsuccessful. Key not found.");
-    else
-        printf("\nSearch successful. Key found at position %d.", loc + 1);
+    if (!found)
+        printf("Key not found\n");
 }
